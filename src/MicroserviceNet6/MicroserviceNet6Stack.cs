73acdb5fbde56cffa239171af4e3d059f7ad4fe3
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2022-03-21 13:55:46 +0100

    Initial commit

---
+++ src/MicroserviceNet6/MicroserviceNet6Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace MicroserviceNet6
+{
+    public class MicroserviceNet6Stack : Stack
+    {
+        internal MicroserviceNet6Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
