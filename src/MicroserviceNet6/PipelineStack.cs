6d38ec0aa0d44a32008b3439902b982dd8a6c506
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2022-06-16 17:34:38 +0200

    Added Synth stage.

--- src/MicroserviceNet6/PipelineStack.cs
+++ src/MicroserviceNet6/PipelineStack.cs
@@ -1,5 +1,8 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.Pipelines;
 using Constructs;
 
 namespace MicroserviceNet6
@@ -12,6 +15,31 @@ namespace MicroserviceNet6
             var repo = new Repository(this, "MicroserviceRepo", new RepositoryProps
             {
                 RepositoryName = "MicroserviceRepo"
+            });
+
+                       // The basic pipeline declaration. This sets the initial structure
+            // of our pipeline
+            var pipeline = new CodePipeline(this, "Pipeline", new CodePipelineProps
+            {
+                PipelineName = "MicroservicePipeline",
+
+                // Builds our source code outlined above into a could assembly artifact
+                Synth = new ShellStep("Synth", new ShellStepProps{
+                    Input = CodePipelineSource.CodeCommit(repo, "master"),  // Where to get source code to build
+                    Commands = new string[] {
+                        "npm install -g aws-cdk",
+                        "wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb",
+                        "sudo dpkg -i packages-microsoft-prod.deb",
+                        "rm packages-microsoft-prod.deb",
+                        "sudo apt-get update",
+                        "sudo apt-get install -y apt-transport-https",
+                        "sudo apt-get update",
+                        "sudo apt-get install -y dotnet-sdk-6.0",
+                        //"sudo apt-get install -y dotnet-sdk-3.1", // Language-specific install cmd
+                        "cd src",
+                        "dotnet build"  // Language-specific build cmd
+                    }
+                }),
             });
         }
     }
