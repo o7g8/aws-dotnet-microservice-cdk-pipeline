ac8763c2000e0247f2b4e05af19104f30b8d964c
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2022-06-16 16:25:53 +0200

    NET6 microservice

---
+++ src/lambdas/ReadPolicy/src/ReadPolicy/Function.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Model;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ReadPolicy
+{
+  public class Function
+  {
+    const string TABLE = "TABLE";
+    private static AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient();
+    private static AmazonLambdaClient lambdaClient;
+
+    static Function()
+    {
+      initialize();
+    }
+
+    static async void initialize()
+    {
+      lambdaClient = new AmazonLambdaClient();
+      await callLambda();
+    }
+
+    public static async Task<GetAccountSettingsResponse> callLambda()
+    {
+      var request = new GetAccountSettingsRequest();
+      var response = await lambdaClient.GetAccountSettingsAsync(request);
+      return response;
+    }
+
+    /// <summary>
+    /// Search for a policy with a given CPR.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns>Policy JSON</returns>
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+    {
+      if (!apigProxyEvent.PathParameters.ContainsKey("cprno"))
+      {
+        return new APIGatewayProxyResponse
+        {
+          Body = "Wrong request",
+          StatusCode = 400,
+        };
+      }
+      var cprNo = Uri.UnescapeDataString(apigProxyEvent.PathParameters["cprno"]);
+      context.Logger.LogLine($"Searching for {cprNo}");
+      try
+      {
+        var response = await dynamoClient.QueryAsync(new QueryRequest()
+        {
+          TableName = System.Environment.GetEnvironmentVariable(TABLE),
+          KeyConditionExpression = "CprNo = :v_Cpr",
+          ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+                        {":v_Cpr", new AttributeValue { S = cprNo }}
+                    },
+          Limit = 1
+        });
+        var responseValue = response.Items.FirstOrDefault();
+        if (responseValue == null)
+        {
+          return new APIGatewayProxyResponse
+          {
+            Body = "Not found",
+            StatusCode = 404,
+          };
+        }
+
+        return new APIGatewayProxyResponse
+        {
+          Body = FormatPolicy(responseValue),
+          StatusCode = 200
+        };
+      }
+      catch (Exception ex)
+      {
+        context.Logger.LogLine(ex.Message);
+        return new APIGatewayProxyResponse
+        {
+          Body = apigProxyEvent.Body,
+          StatusCode = 409
+        };
+      }
+    }
+
+    private string FormatPolicy(Dictionary<string, AttributeValue> responseValue)
+    {
+      var result = new Policy
+      {
+        CprNo = responseValue["CprNo"].S,
+        PolicyOwner = responseValue["PolicyOwner"].S
+      };
+      return JsonSerializer.Serialize(result);
+    }
+  }
+
+  public class Policy
+  {
+    public string CprNo { get; set; }
+    public string PolicyOwner { get; set; }
+  }
+}
--- src/lambdas/SavePolicy/src/SavePolicy/Function.cs
+++ src/lambdas/SavePolicy/src/SavePolicy/Function.cs
@@ -1,14 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
-
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace SavePolicy;
-
-public class Function
+namespace SavePolicy
 {
+  public class Function
+  {
+    private const string TABLE = "TABLE";
+    internal static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+    internal static Table table = Table.LoadTable(client, Environment.GetEnvironmentVariable(TABLE));
+
     /// <summary>
     /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
     /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
@@ -29,17 +38,35 @@ public class Function
     /// <returns></returns>
     public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
     {
-        foreach(var message in evnt.Records)
-        {
-            await ProcessMessageAsync(message, context);
-        }
+      foreach (var message in evnt.Records)
+      {
+        await ProcessMessageAsync(message, context);
+      }
     }
 
     private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
     {
-        context.Logger.LogInformation($"Processed message {message.Body}");
+      context.Logger.LogLine($"Processed message {message.Body}");
 
-        // TODO: Do interesting work based on the new message
-        await Task.CompletedTask;
+      try
+      {
+        var policy = JsonSerializer.Deserialize<Policy>(message.Body);
+        await table.PutItemAsync(new Document {
+                    { "CprNo", policy.CprNo },
+                    { "PolicyOwner", policy.PolicyOwner }
+                });
+      }
+      catch (Exception e)
+      {
+        context.Logger.LogLine($"ERROR: {e.Message}");
+      }
+      await Task.CompletedTask;
     }
+  }
+
+  public class Policy
+  {
+    public string CprNo { get; set; }
+    public string PolicyOwner { get; set; }
+  }
 }
