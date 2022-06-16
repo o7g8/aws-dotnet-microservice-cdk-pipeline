f8e7ad7e443e19ba4a04e57e922ba6dd1c63778d
Oleg Grytsynevych <oleg.gritsinevich@gmail.com>
2022-06-16 19:33:29 +0200

    Simple build commands.

--- src/MicroserviceNet6/PipelineStack.cs
+++ src/MicroserviceNet6/PipelineStack.cs
@@ -27,24 +27,12 @@ namespace MicroserviceNet6
                 Synth = new ShellStep("Synth", new ShellStepProps{
                     Input = CodePipelineSource.CodeCommit(repo, "master"),  // Where to get source code to build
                     InstallCommands = new string[] {
-                        /*
                         "npm install -g aws-cdk",
-                        "wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb",
-                        "sudo dpkg -i packages-microsoft-prod.deb",
-                        "rm packages-microsoft-prod.deb",
-                        "sudo apt-get update",
-                        "sudo apt-get install -y apt-transport-https",
-                        "sudo apt-get update",
-                        "sudo apt-get install -y dotnet-sdk-6.0",
-                        */
                         "/usr/local/bin/dotnet-install.sh --channel LTS",
-                        //"sudo apt-get install -y dotnet-sdk-3.1", // Language-specific install cmd
                     },
                     Commands = new string[] {
-                        "cd src",
-                        "dotnet --version",
-                        "dotnet --list-sdks",
-                        "dotnet build"  // Language-specific build cmd
+                        "cd src && dotnet build",
+                        "cd .. && cdk synth"
                     }
                 }),
             });
